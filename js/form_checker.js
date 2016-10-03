caef43b5fcd893b6f0eeb6988f836968d39bcd62
Grant Jurgensen <gajurgensen@gmail.com>
2016-10-02 23:31:56 -0500

    added css, some of the form_checker

--- js/form_checker.js
+++ js/form_checker.js
@@ -3,11 +3,11 @@ function check(){
   var password = document.getElementById("pass").value;
 
   var u1 = username.split('@');
-  var u2 = u1.split('.')
-  if(!(u1[0].length > 0 && u2[0].length > 0 && u2[1].length > 0)){
+  if(!(u1[0].length > 0 && u1[1].length > 0)){
     alert("Invalid username, must be in the form of an email address.");
     return false;
   }
 
+  return false;
   return true;
 }
